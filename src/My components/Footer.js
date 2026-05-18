24ec25e1151977ae1dc06bab897f610c24cb8d65
Malika <malika968203@gmail.com>
2026-05-18 20:10:25 +0530

    Update Final

--- src/My components/Footer.js
+++ src/My components/Footer.js
@@ -4,11 +4,9 @@ export const Footer = () => {
   let footerStyle = {
     position : "relative",
     width : "100%",
-    border : "2px solid red",
     marginTop: "3rem"
   }
   return (
-    
     <footer className="bg-dark text-light py-3" style={footerStyle}>
       <p className="text-center my-3">Copyright &copy; MyTodosList.com</p>
     </footer>
