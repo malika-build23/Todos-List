078c4e493bdaa6384ac45236a64c8f5633edc214
Malika <malika968203@gmail.com>
2026-05-18 15:31:31 +0530

    u

--- src/My components/AddTodo.js
+++ src/My components/AddTodo.js
@@ -1,17 +1,30 @@
 import React from 'react'
 
-export const AddTodo = () => {
+export const AddTodo = (props) => {
+    const [title, setTitle] = React.useState("");
+    const [desc, setDesc] = React.useState("");
+    const submit = (e)=> {
+        e.preventDefault();
+        if(!title || !desc){
+            alert("Title or Description cannot be blank");
+        }
+        else{
+            props.addTodo(title, desc);
+            setTitle("");
+            setDesc("");
+        }
+    }
     return (
         <div className="container my-3">
             <h3>Add a Todo</h3>
-            <form>
+            <form onSubmit={submit}>
                 <div className="mb-3">
                     <label htmlFor="title" className="form-label">Todo Title</label>
-                    <input type="text" className="form-control" id="title" aria-describedby="emailHelp" />
+                    <input type="text" value = {title} onChange = {(e)=>setTitle(e.target.value)} className="form-control" id="title" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="desc" className="form-label">Todo Description</label>
-                    <input type="text" className="form-control" id="desc" />
+                    <input type="text" value = {desc} onChange = {(e)=>setDesc(e.target.value)} className="form-control" id="desc" />
                 </div>
 
                 <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
