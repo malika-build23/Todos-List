6a0456de4664221a28c0e0959a3283c674d537f0
Malika <malika968203@gmail.com>
2026-05-18 15:20:10 +0530

    Add todo

--- src/My components/AddTodo.js
+++ src/My components/AddTodo.js
@@ -6,19 +6,15 @@ export const AddTodo = () => {
             <h3>Add a Todo</h3>
             <form>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <label htmlFor="title" className="form-label">Todo Title</label>
+                    <input type="text" className="form-control" id="title" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <label htmlFor="desc" className="form-label">Todo Description</label>
+                    <input type="text" className="form-control" id="desc" />
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+
+                <button type="submit" className="btn btn-sm btn-success">Add Todo</button>
             </form>
         </div>
     )
